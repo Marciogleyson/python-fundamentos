4f2052b673560c55cce7875c1f3187b7c3a0f538
Marciogleyson <marcio.gleyson2026@gmail.com>
2025-03-12 18:04:59 -0300

    Update exemplo_01_estrutura_condicional.py

--- exemplo_01_estrutura_condicional.py
+++ exemplo_01_estrutura_condicional.py
@@ -47,24 +47,44 @@ def exemplo_if_operador_e():
     print("Valor desconto: ", valor_desconto)
     print("Valor total: ", valor_total_compra)
 
-    def exemplo_if_operador_ou():
-        nome = input("Digite o nome do jogo: ")
-        categoria = input("Digite a categoria: ")
 
-        if categoria == "RPG" or categoria == "Ação":
-            preco = 150.99
-        elif categoria == "eSports" or categoria == "Moba":
+def exemplo_if_operador_ou():
+    nome = input("Digite o nome do jogo: ").strip()
+    categoria = input("Digite a categoria: ").strip()
+    
+    categoria_sanitizada = categoria.lower()
+
+    if categoria_sanitizada == "rpg" or categoria_sanitizada == "Ação":
+        preco = 150.99
+    elif categoria_sanitizada == "esports" or categoria_sanitizada == "moba":
             preco = 99.99
-        else:
-            preco = 200.00
+    else:
+        preco = 200.00
 
+    print("Nome: ", nome)
+    print("categoria: ", categoria)
     print("Preço:", preco)
-    #Tabela verdade operador lógico ou
-    # V ou V -> V
-    # V ou F -> V
-    # F ou V -> v
-    # F ou F -> F                
-
+            #tabela verdade operador lógico  ou
+            # V ou V -> V
+            # V ou F -> V
+            # F ou V -> V
+            # F ou F -> F
+            
+def verificar_numero_par():
+    numero = int(input("Digite o número: "))
+    if numero % 2 == 0: #se o resto da divisão é igual a 0
+        print("par")
+    else:
+        print("Não é par")
+        
+        
+def verificar_numero_impar():
+    numero = int(input("Digite o número:  "))
+    if numero % 2 != 0: #se o resto da divisão é diferente de 0
+        print("Ímpar")
+    else:
+        print("Não é ímpar")
+        
 
 if __name__ == "__main__":
     exemplo_if_operador_ou()
